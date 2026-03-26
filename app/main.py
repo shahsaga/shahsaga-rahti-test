91d37e8f0a2d0d8650b720ba5fe9aae0dabe8321
shahsaga <shahsagar0012@gmail.com>
2026-03-26 20:13:56 +0545

    made some changes

--- app/main.py
+++ app/main.py
@@ -4,7 +4,7 @@ app = FastAPI()
 
 @app.get("/")
 def read_root():
-    return { "msg": "Hello! SAGAR Shah", "v": "0.1" }
+    return { "msg": "Final version", "v": "0.1" }
 
 
 @app.get("/items/{id}")
